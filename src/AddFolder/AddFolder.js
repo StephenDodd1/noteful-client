1d2d04c9950d52ae64ca2b0df42b3686b0096c06
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-06-03 18:26:02 -0500

    Added NoteListNav component for back button

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import NotePageNav from '../NotePageNav/NotePageNav';
 
 export default class AddFolder extends Component {
+   constructor(props) {
+      super(props);
+      this.nameInput = React.createRef();
+   }
    render() {
+
       return(
-         <p>HelloWorld</p>
+         <div className='note-page-container'>
+         <NotePageNav />
+         <form className='add-folder-form'>
+            <input 
+            type ='text' 
+            placeholder='Folder Name' 
+            defaultValue='newFolder' />
+         </form>
+         </div>
       )
    }
 }
--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
+import AddFolder from '../AddFolder/AddFolder'
 import { countNotesForFolder } from '../notes-helpers'
 import './NoteListNav.css'
 
