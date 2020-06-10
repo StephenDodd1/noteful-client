bbb437e84379902baf847c4e139d331badc43be9
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-06-09 23:17:46 -0500

    fixed AddFolder back button

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -6,9 +6,15 @@ export default class AddFolder extends Component {
    state={}
 
    handleCreateFolder = (e) => {
+      const appFolders = this.props.folders
       e.preventDefault();
       const { folderName } = this.state;
+      for(let i = 0; i < appFolders.length; i++) {
+         if(folderName === appFolders[i].name) {
+            return (alert('There is already a folder with this name.'))}
+         }
       this.props.handleSubmit(folderName);
+      this.props.handleCancel()
    }
 
    onFolderNameChange = ({target}) => {
@@ -30,7 +36,7 @@ export default class AddFolder extends Component {
       return(
          <div className='folder-page-container'>
             <NotePageNav 
-               handleCancel ={this.handleCancel} />
+               handleCancel = {this.props.handleCancel} />
                <label htmlFor='add-folder-form'>Add a folder: </label>
             <form className='add-folder-form' id='add-folder-form' onSubmit={e => this.handleCreateFolder(e)}>
                <label htmlFor='folder-name-input'>Folder Name: <span>&emsp;</span></label>
@@ -40,6 +46,7 @@ export default class AddFolder extends Component {
                   type ='text' 
                   name='folderName'
                   placeholder='Folder Name' 
+                  required
                /><br />
                <button type='submit' >Submit</button>
             </form>
